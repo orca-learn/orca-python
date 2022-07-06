1cf9adb8f9aee22aa349dfb9414acd3c5c8c9179
orca-learn <108380344+orca-learn@users.noreply.github.com>
2022-07-06 14:41:29 -0400

    02-syntax

---
+++ 02-syntax.py
@@ -0,0 +1,26 @@
+# https://www.w3schools.com/python/python_syntax.asp
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+ 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+
+if 5 > 2:
+ print("Five is greater than two!")
+ print("Five is greater than two!")
+
+
+x = 5
+y = "Hello, World!"
+
+
+#This is a comment.
+print("Hello, World!")
