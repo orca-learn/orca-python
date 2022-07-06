5e297527cf4b49b1b0d7c4c4b6dce08a00ac284b
orca-learn <108380344+orca-learn@users.noreply.github.com>
2022-07-06 14:43:35 -0400

    03-comments

---
+++ 03-comments.py
@@ -0,0 +1,23 @@
+# https://www.w3schools.com/python/python_comments.asp
+
+
+#This is a comment
+print("Hello,World!")
+
+print("Hello, World!") #This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
